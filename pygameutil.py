6a941a4cf322c71d2476e6078ecc87d13068c23e
Evan Finken <evan@finken.name>
2022-06-02 22:06:53 -0500

    Add more sequence methods, implement deleting items

--- pygameutil.py
+++ pygameutil.py
@@ -56,35 +56,12 @@ class Button:
 
 class GridBoard:
     def __init__(self, row, col, *, fill=None, grid=None, default_grid=None):
-        # instead of using fill for __delitem__, just replace item at that index with the item in default_grid
-        self.default_grid = [[fill] * col] * row if default_grid is None else default_grid
-        self.grid = self.default_grid.copy() if grid is None else grid
+        # instead of using fill for __delitem__, just replace item at that index with the item in default
+        self.default = [[fill] * col] * row if default_grid is None else default_grid
+        self.grid = self.default.copy() if grid is None else grid
 
-    def reset(self):
-        self.grid = self.default_grid.copy()
-
-    def delete(self, row, col):
-        self.grid[row][col] = self.default_grid[row][col]
-
-    def get_pos(self, mouse_pos, screen_size, pos_length):
-        pos = -1
-        if 0 < mouse_pos[0] < screen_size[0] and 0 < mouse_pos[1] < screen_size[1]:
-            row = mouse_pos[1] // pos_length
-            col = mouse_pos[0] // pos_length
-            if 0 <= row < len(self.grid) and 0 <= col < len(self.grid[0]):
-                pos = (row, col)
-        return pos
-
-    def line(self, *args):
-        if len(args) == 0:
-            raise TypeError("line expected at least 1 argument, got 0")
-        start, stop, step = (0, 0), (0, 0), (1, 1)
-        x_indices = [x for x in range(start[0], stop[0], step[0])]
-        y_indices = [y for y in range(start[1], stop[1], step[1])]
-        items = []
-        for k in range(min(len(x_indices), len(y_indices))):
-            items.append(self.grid[x_indices[k]][y_indices[k]])
-        return items
+    def __len__(self):
+        return len(self.grid)
 
     def __contains__(self, value):
         return any(value in row for row in self.grid)
@@ -102,3 +79,58 @@ class GridBoard:
             return sliced
         else:
             return self.grid[key]
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            pass
+
+    def __delitem__(self, *args):
+        if isinstance(args[0], tuple):
+            args = args[0]
+        if len(args) == 0:
+            self.reset()
+        elif len(args) == 1:
+            self.grid[args[0]] = self.default[args[0]].copy()
+        else:
+            if isinstance(args[0], slice):
+                sliced_grid = self.grid[args[0]]
+                sliced_default = self.default[args[0]]
+                for i in range(len(sliced_grid)):
+                    sliced_grid[i][args[1]] = sliced_default[i][args[1]]
+            else:
+                self.grid[args[0]][args[1]] = self.default[args[0]][args[1]]
+
+    def __iter__(self):
+        return iter(self.grid)
+
+    def __reversed__(self):
+        return reversed(self.grid)
+
+    def reset(self):
+        self.grid = self.default.copy()
+
+    def delete(self, *args):
+        self.__delitem__(*args)
+
+    def get_pos(self, mouse_pos, screen_size, pos_length):
+        pos = -1
+        if 0 < mouse_pos[0] < screen_size[0] and 0 < mouse_pos[1] < screen_size[1]:
+            row = mouse_pos[1] // pos_length
+            col = mouse_pos[0] // pos_length
+            if 0 <= row < len(self.grid) and 0 <= col < len(self.grid[0]):
+                pos = (row, col)
+        return pos
+
+    def line(self, *args):
+        if len(args) == 0:
+            raise TypeError("line expected at least 1 argument, got 0")
+        start, stop, step = (0, 0), (0, 0), (1, 1)
+        if len(args) == 1:
+            pass
+
+        x_indices = [x for x in range(start[0], stop[0], step[0])]
+        y_indices = [y for y in range(start[1], stop[1], step[1])]
+        items = []
+        for k in range(min(len(x_indices), len(y_indices))):
+            items.append(self.grid[x_indices[k]][y_indices[k]])
+        return items
